e34cd1335bb81d6607221adb9f8080aa68518cdf
RD090911 <rdrooger1@gmail.com>
2026-01-21 20:49:07 -0500

    Initial commit

---
+++ src/main/java/me/druid/v1/DruidPlugin.java
@@ -0,0 +1,38 @@
+package me.druid.v1;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.event.EventRegistry;
+import com.hypixel.hytale.server.core.event.events.player.PlayerConnectEvent;
+import com.hypixel.hytale.logger.HytaleLogger;
+import javax.annotation.Nonnull;
+
+public class DruidPlugin extends JavaPlugin {
+
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public DruidPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        LOGGER.atInfo().log("DruidFormV1 is setting up...");
+        EventRegistry.register(this, this);
+    }
+
+    @Override
+    protected void start() {
+        LOGGER.atInfo().log("DruidFormV1 is ready!");
+    }
+
+    @Override
+    protected void shutdown() {
+        LOGGER.atInfo().log("DruidFormV1 is shutting down.");
+    }
+
+    // Notice: NO @Annotation here anymore!
+    public void onPlayerConnect(PlayerConnectEvent event) {
+        LOGGER.atInfo().log("A Druid has connected to the server!");
+    }
+}
