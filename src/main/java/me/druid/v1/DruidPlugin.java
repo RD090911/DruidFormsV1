e13bef806520508e4f843eb37a8c586fef0a1b6e
RD090911 <rdrooger1@gmail.com>
2026-01-24 09:51:54 -0500

    Fixing DruidPlugin.java

--- src/main/java/me/druid/v1/DruidPlugin.java
+++ src/main/java/me/druid/v1/DruidPlugin.java
@@ -1,26 +1,33 @@
 package me.druid.v1;
 
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import java.util.logging.Logger;
 
 public class DruidPlugin extends JavaPlugin {
-    private final Logger logger;
     private ShapeshiftHandler handler;
     private ShapeshiftCommand command;
+    private Logger logger;
 
-    // We bring back the constructor because Hytale NEEDS it to inject the logger!
-    public DruidPlugin(Logger logger) {
-        this.logger = logger;
+    // The Constructor: The "Key" the server uses to start us.
+    public DruidPlugin(JavaPluginInit init) {
+        super(init);
     }
 
+    // FIXED: The official Hytale API method is 'start()', NOT 'onStart'
     @Override
-    public void onEnable() {
-        // Now we use the local 'logger' variable again, which fixes the red lines
-        this.handler = new ShapeshiftHandler(logger);
+    public void start() {
+        // Create our own logger to be safe
+        this.logger = Logger.getLogger("DruidFormsV1");
+
+        this.logger.info("Initializing Druid Plugin...");
+
+        // Initialize the handler with our logger
+        this.handler = new ShapeshiftHandler(this.logger);
 
         // Initialize the command
         this.command = new ShapeshiftCommand(handler);
 
-        logger.info("Druid Plugin has been enabled with commands!");
+        this.logger.info("Druid Plugin has been enabled with commands!");
     }
 }
