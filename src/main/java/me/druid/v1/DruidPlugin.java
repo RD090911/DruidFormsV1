6103486f14f3f74fbee4bccd7174958722e62201
RD090911 <rdrooger1@gmail.com>
2026-01-24 08:58:18 -0500

    Fixing DruidPlugin.java

--- src/main/java/me/druid/v1/DruidPlugin.java
+++ src/main/java/me/druid/v1/DruidPlugin.java
@@ -1,20 +1,24 @@
 package me.druid.v1;
 
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import java.util.logging.Logger;
 
-public class DruidPlugin {
+public class DruidPlugin extends JavaPlugin {
     private final Logger logger;
     private ShapeshiftHandler handler;
-    private ShapeshiftCommand command; // Add this line
+    private ShapeshiftCommand command;
 
+    // We bring back the constructor because Hytale NEEDS it to inject the logger!
     public DruidPlugin(Logger logger) {
         this.logger = logger;
     }
 
+    @Override
     public void onEnable() {
+        // Now we use the local 'logger' variable again, which fixes the red lines
         this.handler = new ShapeshiftHandler(logger);
 
-        // Initialize the command and give it access to the handler
+        // Initialize the command
         this.command = new ShapeshiftCommand(handler);
 
         logger.info("Druid Plugin has been enabled with commands!");
